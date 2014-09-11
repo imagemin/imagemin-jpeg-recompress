0566d60fb77ad22d190802eb779d4a3cd157ca58
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2014-09-11 11:39:18 +0900

    Return transform stream instead of function

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
 var isJpg = require('is-jpg');
 var jpegRecompress = require('jpeg-recompress-bin').path;
 var spawn = require('child_process').spawn;
+var through = require('through2');
 
 /**
- * jpegrecompress image-min plugin
+ * jpegrecompress imagemin plugin
  *
  * @param {Object} opts
  * @api public
@@ -13,53 +14,63 @@ var spawn = require('child_process').spawn;
 
 module.exports = function (opts) {
 	opts = opts || {};
+	var args = ['-', '-', '-s'];
 
-	return function (file, imagemin, cb) {
-		if (!isJpg(file.contents)) {
-			cb();
+	if (opts.accurate) {
+		args.push('-a');
+	}
+
+	if (opts.quality) {
+		args.push('-q', opts.quality);
+	}
+
+	if (opts.method) {
+		args.push('-m', opts.method);
+	}
+
+	if (opts.target) {
+		args.push('-t', opts.target);
+	}
+
+	if (opts.min) {
+		args.push('-n', opts.min);
+	}
+
+	if (opts.max) {
+		args.push('-x', opts.max);
+	}
+
+	if (opts.loops) {
+		args.push('-l', opts.loops);
+	}
+
+	if (opts.defish) {
+		args.push('-d', opts.defish);
+	}
+
+	if (opts.zoom) {
+		args.push('-z', opts.zoom);
+	}
+
+	return through.obj(function (file, enc, cb) {
+		if (file.isNull()) {
+			cb(null, file);
 			return;
 		}
 
-		var args = ['-', '-', '-s'];
+		if (file.isStream()) {
+			cb(new Error('Streaming is not supported'));
+			return;
+		}
+
+		if (!isJpg(file.contents)) {
+			cb(null, file);
+			return;
+		}
+
 		var err = '';
 		var ret = [];
 		var len = 0;
-
-		if (opts.accurate) {
-			args.push('-a');
-		}
-
-		if (opts.quality) {
-			args.push('-q', opts.quality);
-		}
-
-		if (opts.method) {
-			args.push('-m', opts.method);
-		}
-
-		if (opts.target) {
-			args.push('-t', opts.target);
-		}
-
-		if (opts.min) {
-			args.push('-n', opts.min);
-		}
-
-		if (opts.max) {
-			args.push('-x', opts.max);
-		}
-
-		if (opts.loops) {
-			args.push('-l', opts.loops);
-		}
-
-		if (opts.defish) {
-			args.push('-d', opts.defish);
-		}
-
-		if (opts.zoom) {
-			args.push('-z', opts.zoom);
-		}
 
 		var cp = spawn(jpegRecompress, args);
 
@@ -85,9 +96,9 @@ module.exports = function (opts) {
 			}
 
 			file.contents = Buffer.concat(ret, len);
-			cb();
+			cb(null, file);
 		});
 
 		cp.stdin.end(file.contents);
-	};
+	});
 };
--- test/test.js
+++ test/test.js
@@ -1,57 +1,87 @@
 'use strict';
 
+var bufferEqual = require('buffer-equal');
+var File = require('vinyl');
 var fs = require('fs');
-var Imagemin = require('imagemin');
 var isJpg = require('is-jpg');
 var jpegRecompress = require('../');
 var path = require('path');
 var test = require('ava');
 
 test('optimize a JPG', function (t) {
-	t.plan(4);
+	t.plan(3);
 
-	var imagemin = new Imagemin()
-		.src(path.join(__dirname, 'fixtures/test.jpg'))
-		.use(jpegRecompress());
-
-	imagemin.optimize(function (err, file) {
+	fs.readFile(path.join(__dirname, 'fixtures/test.jpg'), function (err, buf) {
 		t.assert(!err);
 
-		fs.stat(imagemin.src(), function (err, stats) {
-			t.assert(!err);
-			t.assert(file.contents.length < stats.size);
-			t.assert(isJpg(file.contents));
+		var stream = jpegRecompress();
+		var file = new File({
+			contents: buf
 		});
+
+		stream.on('data', function (data) {
+			t.assert(data.contents.length < buf.length);
+			t.assert(isJpg(data.contents));
+		});
+
+		stream.end(file);
+	});
+});
+
+test('skip optimizing a non-JPG file', function (t) {
+	t.plan(2);
+
+	fs.readFile(__filename, function (err, buf) {
+		t.assert(!err);
+
+		var stream = jpegRecompress();
+		var file = new File({
+			contents: buf
+		});
+
+		stream.on('data', function (data) {
+			t.assert(bufferEqual(data.contents, buf));
+		});
+
+		stream.end(file);
 	});
 });
 
 test('skip optimizing an already optimized JPG', function (t) {
-	t.plan(4);
+	t.plan(2);
 
-	var imagemin = new Imagemin()
-		.src(path.join(__dirname, 'fixtures/test-smallest.jpg'))
-		.use(jpegRecompress());
-
-	imagemin.optimize(function (err, file) {
+	fs.readFile(path.join(__dirname, 'fixtures/test-smallest.jpg'), function (err, buf) {
 		t.assert(!err);
 
-		fs.stat(imagemin.src(), function (err, stats) {
-			t.assert(!err);
-			t.assert(file.contents.length === stats.size);
-			t.assert(isJpg(file.contents));
+		var stream = jpegRecompress();
+		var file = new File({
+			contents: buf
 		});
+
+		stream.on('data', function (data) {
+			t.assert(bufferEqual(data.contents, buf));
+		});
+
+		stream.end(file);
 	});
 });
 
 test('throw error when a JPG is corrupt', function (t) {
-	t.plan(2);
+	t.plan(3);
 
-	var imagemin = new Imagemin()
-		.src(path.join(__dirname, 'fixtures/test-corrupt.jpg'))
-		.use(jpegRecompress());
+	fs.readFile(path.join(__dirname, 'fixtures/test-corrupt.jpg'), function (err, buf) {
+		t.assert(!err);
 
-	imagemin.optimize(function (err) {
-		t.assert(err);
-		t.assert(/Corrupt JPEG data/.test(err.message));
+		var stream = jpegRecompress();
+		var file = new File({
+			contents: buf
+		});
+
+		stream.on('error', function (err) {
+			t.assert(err);
+			t.assert(/Corrupt JPEG data/.test(err.message));
+		});
+
+		stream.end(file);
 	});
 });
