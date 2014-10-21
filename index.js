8a11c8d1367c84a802460d50662cf3d2b326271b
Kevin Martensson <kevinmartensson@gmail.com>
2014-10-21 16:26:16 +0200

    Provide a reusable constructor

--- index.js
+++ index.js
@@ -8,6 +8,103 @@ var through = require('through2');
 /**
  * jpegrecompress imagemin plugin
  *
+ * @param {Object} file
+ * @param {String} enc
+ * @param {Object} opts
+ * @param {Function} cb
+ * @api private
+ */
+
+function plugin(file, enc, opts, cb) {
+	if (file.isNull()) {
+		cb(null, file);
+		return;
+	}
+
+	if (file.isStream()) {
+		cb(new Error('Streaming is not supported'));
+		return;
+	}
+
+	if (!isJpg(file.contents)) {
+		cb(null, file);
+		return;
+	}
+
+	var args = ['-', '-', '-s'];
+	var err = '';
+	var ret = [];
+	var len = 0;
+
+	if (opts.accurate) {
+		args.push('-a');
+	}
+
+	if (opts.quality) {
+		args.push('-q', opts.quality);
+	}
+
+	if (opts.method) {
+		args.push('-m', opts.method);
+	}
+
+	if (opts.target) {
+		args.push('-t', opts.target);
+	}
+
+	if (opts.min) {
+		args.push('-n', opts.min);
+	}
+
+	if (opts.max) {
+		args.push('-x', opts.max);
+	}
+
+	if (opts.loops) {
+		args.push('-l', opts.loops);
+	}
+
+	if (opts.defish) {
+		args.push('-d', opts.defish);
+	}
+
+	if (opts.zoom) {
+		args.push('-z', opts.zoom);
+	}
+
+	var cp = spawn(jpegRecompress, args);
+
+	cp.on('error', function (err) {
+		cb(err);
+		return;
+	});
+
+	cp.stderr.setEncoding('utf8');
+	cp.stderr.on('data', function (data) {
+		err += data;
+	});
+
+	cp.stdout.on('data', function (data) {
+		ret.push(data);
+		len += data.length;
+	});
+
+	cp.on('close', function (code) {
+		if (code) {
+			cb(new Error(err));
+			return;
+		}
+
+		file.contents = Buffer.concat(ret, len);
+		cb(null, file);
+	});
+
+	cp.stdin.end(file.contents);
+}
+
+/**
+ * Module exports
+ *
  * @param {Object} opts
  * @api public
  */
@@ -16,89 +113,21 @@ module.exports = function (opts) {
 	opts = opts || {};
 
 	return through.obj(function (file, enc, cb) {
-		if (file.isNull()) {
-			cb(null, file);
-			return;
-		}
+		plugin(file, enc, opts, cb);
+	});
+};
 
-		if (file.isStream()) {
-			cb(new Error('Streaming is not supported'));
-			return;
-		}
+/**
+ * Module exports constructor
+ *
+ * @param {Object} opts
+ * @api public
+ */
 
-		if (!isJpg(file.contents)) {
-			cb(null, file);
-			return;
-		}
+module.exports.ctor = function (opts) {
+	opts = opts || {};
 
-		var args = ['-', '-', '-s'];
-		var err = '';
-		var ret = [];
-		var len = 0;
-
-		if (opts.accurate) {
-			args.push('-a');
-		}
-
-		if (opts.quality) {
-			args.push('-q', opts.quality);
-		}
-
-		if (opts.method) {
-			args.push('-m', opts.method);
-		}
-
-		if (opts.target) {
-			args.push('-t', opts.target);
-		}
-
-		if (opts.min) {
-			args.push('-n', opts.min);
-		}
-
-		if (opts.max) {
-			args.push('-x', opts.max);
-		}
-
-		if (opts.loops) {
-			args.push('-l', opts.loops);
-		}
-
-		if (opts.defish) {
-			args.push('-d', opts.defish);
-		}
-
-		if (opts.zoom) {
-			args.push('-z', opts.zoom);
-		}
-
-		var cp = spawn(jpegRecompress, args);
-
-		cp.on('error', function (err) {
-			cb(err);
-			return;
-		});
-
-		cp.stderr.setEncoding('utf8');
-		cp.stderr.on('data', function (data) {
-			err += data;
-		});
-
-		cp.stdout.on('data', function (data) {
-			ret.push(data);
-			len += data.length;
-		});
-
-		cp.on('close', function (code) {
-			if (code) {
-				cb(new Error(err));
-				return;
-			}
-
-			file.contents = Buffer.concat(ret, len);
-			cb(null, file);
-		});
-
-		cp.stdin.end(file.contents);
+	return through.ctor({ objectMode: true }, function (file, enc, cb) {
+		plugin(file, enc, opts, cb);
 	});
 };
