e7dbd70dd6207a04b4551b01318ea29b539a43a6
Aditya Patadia <adityapatadia@users.noreply.github.com>
2018-11-30 23:23:46 +0530

    Update test.js

--- test.js
+++ test.js
@@ -36,7 +36,7 @@ test('strip metadata by default', async t => {
 	const buf = await fsP.readFile(path.join(__dirname, 'fixture.jpg'));
 	const data = await m()(buf);
 
-	t.throws(pify(exifImage)(data), 'No Exif segment found in the given image.');
+	t.throws(await pify(exifImage)(data), 'No Exif segment found in the given image.');
 });
 
 test('skip optimizing a non-JPG file', async t => {
@@ -48,5 +48,5 @@ test('skip optimizing a non-JPG file', async t => {
 
 test('throw error when a JPG is corrupt', async t => {
 	const buf = await fsP.readFile(path.join(__dirname, 'fixture-corrupt.jpg'));
-	t.throws(m()(buf), /Corrupt JPEG data/);
+	t.throws(await m()(buf), /Corrupt JPEG data/);
 });
