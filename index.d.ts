890a5b219aa90c1b550a251f591703e02cb9bc45
Sindre Sorhus <sindresorhus@gmail.com>
2023-01-26 11:22:37 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -4,77 +4,77 @@ export type Options = {
 
 	@default false
 	*/
-	accurate?: boolean;
+	readonly accurate?: boolean;
 
 	/**
 	Set a quality preset.
 
 	@default 'medium'
 	*/
-	quality?: 'low' | 'medium' | 'high' | 'veryhigh';
+	readonly quality?: 'low' | 'medium' | 'high' | 'veryhigh';
 
 	/**
-	Set [comparison method](https://github.com/danielgtaylor/jpeg-archive#image-comparison-metrics)
+	Set [comparison method](https://github.com/danielgtaylor/jpeg-archive#image-comparison-metrics).
 
 	@default 'ssim'
 	*/
-	method?: 'mpe' | 'ssim' | 'ms-ssim' | 'smallfry';
+	readonly method?: 'mpe' | 'ssim' | 'ms-ssim' | 'smallfry';
 
 	/**
 	Set target quality.
 
 	@default 0.999
 	*/
-	target?: number;
+	readonly target?: number;
 
 	/**
 	Minimum JPEG quality.
 
 	@default 40
 	*/
-	min?: number;
+	readonly min?: number;
 
 	/**
 	Maximum JPEG quality.
 
 	@default 95
 	*/
-	max?: number;
+	readonly max?: number;
 
 	/**
 	Set the number of attempts.
 
 	@default 6
 	*/
-	loops?: number;
+	readonly loops?: number;
 
 	/**
 	Set defish strength.
 
 	@default 0
 	*/
-	defish?: number;
+	readonly defish?: number;
 
 	/**
 	Enable progressive encoding.
 
 	@default true
 	*/
-	progressive?: boolean;
+	readonly progressive?: boolean;
 
 	/**
 	Set subsampling method. Available values: `default`, `disable`.
 
 	@default 'default'
 	*/
-	subsample?: 'default' | 'disable';
+	readonly subsample?: 'default' | 'disable';
 
 	/**
 	Strips metadata, such as EXIF data.
 
 	@default true
 	*/
-	strip?: boolean;
+	readonly strip?: boolean;
 };
 
 /**
